c60791a034c7d7810ee7c8cbe519a1fc7bac18ed
ShadowMoonV <834540085@qq.com>
2017-07-17 19:43:03 +0800

    完善了登陆模块，增加了数据库验证

--- FlaskTest.py
+++ FlaskTest.py
@@ -8,22 +8,13 @@ app.config.from_object(__name__)
 
 
 app.config.update(dict(
-    SECRET_KEY = 'key',
+    SECRET_KEY = 'This is a secret key, which is a secret.',
 ))
 app.config.from_envvar('FLASKETEST_SETTINGS',silent=True)
 
 def connect_db():
     conn = mysql.connector.connect(user='root',password='8749654',database='bilibili')
     return conn
-
-"""
-def init_db():
-    with app.app_context():
-        db = get_db()
-        with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
-"""
 
 def get_db():
     if not hasattr(g,'mysql_db'):
@@ -39,7 +30,6 @@ def close_db(erro):
 @app.route('/')
 def show_entries():
     db = get_db()
-    ##cur = db.execute('SELECT title,content FROM entries ORDER BY id DESC ')
     sql = "select user_name,videotag,weight " \
           "from userinfo,userprf " \
           "where userinfo.user_id = userprf.user_id;"
@@ -54,13 +44,11 @@ def register():
     if not session.get('logged_in'):
         abort(401)
     db = get_db()
-    ##db.execute('insert into entries (title, content) values (?, ?)',
-    ##             [request.form['title'], request.form['content']])
     sql = "insert into userinfo (user_name,user_password) values (%s,%s)"
     cur = db.cursor()
     cur.execute(sql,[request.form['user_name'],request.form['user_password']])
     db.commit()
-    flash('New user was successfully registed!')
+    flash('注册成功！')
     return redirect(url_for('show_entries'))
 
 
@@ -70,13 +58,21 @@ def login():
     if request.method == 'POST':
         error = None
         if request.method == 'POST':
-            if  request.form['username'] != 'admin':
-                error = 'Invalid username'
-            elif request.form['password'] != 'default':
-                error = 'Invalid password'
+            u = request.form['username']
+            p = request.form['password']
+            db =get_db()
+            cur = db.cursor()
+            cur.execute('select user_name,user_password from userinfo where user_name = %s', (u,))
+            content = cur.fetchone()
+            username = content[0]
+            password = content[1]
+            if  u != username:
+                error = '用户名或密码错误'
+            elif p != password:
+                error = '用户名或密码错误'
             else:
                 session['logged_in'] = True
-                flash('You were logged in')
+                flash('登录成功')
                 return redirect(url_for('show_entries'))
     return render_template('login.html',error=error)
 
@@ -84,7 +80,7 @@ def login():
 @app.route('/logout')
 def logout():
     session.pop('logged_in',None)
-    flash('You were logged out')
+    flash('已注销登录')
     return redirect(url_for('show_entries'))
 
 if __name__ == '__main__':
